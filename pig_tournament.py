c23a6e38bd6be7dd5da3d5666a03d6edff430d42
Steven Pav <shabbychef@gmail.com>
2024-05-16 19:54:11 -0700

    optimal strategy via dynamic programming

--- pig_tournament.py
+++ pig_tournament.py
@@ -15,6 +15,7 @@
 
 import pig
 import functools
+from collections import defaultdict
 
 def strategy_tally_limit(tally, scores, winscore, tally_limit=20):
     return tally >= tally_limit
@@ -72,6 +73,90 @@ def example_tournament_2(max_reps=10_000):
 
 tourney = example_tournament_2(40_000)
 tourney.win_history
+
+# winprobs is a dict with keys as tuples:
+# (myscore, theirscore, mytally) and points to (probwin, best_action)
+# we prepopulate it with approximate winprobs.
+winprobs = defaultdict(lambda : (0.5, None))  # not sure this is necessary
+winscore = 100
+for myscore in range(winscore):
+    for theirscore in range(winscore):
+        for tally in range(winscore-myscore):
+            probwin = 0.03 + (0.5 * (1 + ((myscore + tally) - theirscore) / winscore))
+            probwin += 0.3 * (myscore+tally)/winscore
+            probwin = max(0,min(1,probwin))
+            winprobs[(myscore, theirscore, tally)] = (probwin, None)
+
+def do_dp_updates(winprobs, other_probs=None, min_change=0.002, max_it=100, sentinel_keys=[(0,0,0), (30,30,0), (91,91,1)]):
+    if other_probs is None:
+        other_probs = pig.GamePayoff.pmf_dict
+    bacon_prob = other_probs['BACON']
+    zero_prob = other_probs[0]
+    del other_probs['BACON']
+    del other_probs[0]
+    sentinel = {key:winprobs[key][0] for key in sentinel_keys}
+    iterates = 0
+    done_iterates = iterates >= max_it
+    converged = False
+    while not done_iterates and not converged:
+        for myscore in range(winscore,-1,-1):
+            for theirscore in range(winscore,-1,-1):
+                for tally in range(winscore-myscore,-1,-1):
+                    pass_lose_prob, _ = winprobs[(theirscore, myscore+tally, 0)]
+                    pass_win_prob = 1 - pass_lose_prob
+                    # now the probability of winning when you roll again.
+                    roll_win_prob = 0
+                    bacon_lose_prob, _ = winprobs[(theirscore, 0, 0)] 
+                    roll_win_prob += bacon_prob * (1 - bacon_lose_prob)
+                    pigout_lose_prob, _ = winprobs[(theirscore, myscore, 0)] 
+                    roll_win_prob += zero_prob * (1 - pigout_lose_prob)
+                    for points, prob in other_probs.items():
+                        if myscore + tally + points >= winscore:
+                            this_win_prob = 1
+                        else:
+                            this_win_prob, _ = winprobs[(myscore, theirscore, tally + points)] 
+                        roll_win_prob += prob * this_win_prob
+                    # we now have the pass_win_prob and the roll_win_prob; use these.
+                    if pass_win_prob > roll_win_prob:
+                        winprobs[(myscore, theirscore, tally)] = (pass_win_prob, 'pass')
+                    else:
+                        winprobs[(myscore, theirscore, tally)] = (roll_win_prob, 'roll')
+        iterates += 1
+        done_iterates = iterates >= max_it
+        next_sentinel = {key:winprobs[key][0] for key in sentinel_keys}
+        converged = max([abs(next_sentinel[key] - value) for key, value in sentinel.items()]) < min_change
+        print(f"{iterates=} {sentinel=} {next_sentinel=}")
+        sentinel = next_sentinel
+    return winprobs
+
+winprobs = do_dp_updates(winprobs, min_change=0.0001, max_it=100)
+
+for k,v in winprobs.items():
+    if v[1] is None:
+        print(f"missing for {k}")
+        break
+
+def strategy_optimal(tally, scores, winscore, winprobs):
+    myscore = scores[0]
+    theirscore = max(scores[1:])
+    _, strat = winprobs[(myscore, theirscore, tally)]
+    return strat == "pass"
+
+def example_tournament_3(max_reps=10_000):
+    tourney = pig.PassThePigsTournament(
+        bots=[
+            functools.partial(strategy_tally_limit, tally_limit=32),
+            functools.partial(strategy_optimal, winprobs=winprobs),
+            strategy_weird,
+            strategy_expected_value_limit,
+        ]
+    )
+    winner = tourney.tournament(max_reps=max_reps, min_reps=0.5 * max_reps)
+    return tourney
+
+tourney = example_tournament_3(50_000)
+tourney.win_history
+
 
 #for vim modeline: (do not edit)
 # vim:ts=4:sw=4:sts=4:tw=79:sta:et:ai:nu:fdm=indent:syn=python:ft=python:tag=.py_tags;:cin:fo=croql
