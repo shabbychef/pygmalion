a455b738d85dc51962ef6f72502f51a9aad5460c
Steven Pav <shabbychef@gmail.com>
2024-05-18 17:17:53 -0700

    connect four

--- turn_based.py
+++ turn_based.py
@@ -119,6 +119,117 @@ class TicTacToeGame(AbstractTurnBasedGame):
         breps = [reps[x] for x in self.__board]
         return "|".join(breps[:3]) + "\n" + "|".join(["---"]*3) + "\n" + "|".join(breps[3:6]) + "\n" + "|".join(["---"]*3) + "\n" + "|".join(breps[6:9])
 
+class ConnectFourGame(AbstractTurnBasedGame):
+    def __init__(self, n_connect=4, n_players=2, width=7, height=6, turn=0):
+        super().__init__(turn=turn)
+        self.__n_players = n_players
+        self.__board = [" "] * width * height
+        self.__history = []
+        self.__n_connect = n_connect
+        self.__width = width
+        self.__height = height
+        win_indices = []
+        maxi = width * height
+        # horizontal win indices
+        for rown in range(height):
+            for coln in range(1+width-n_connect):
+                idx = rown*width + coln
+                idx_end = idx + n_connect
+                win_indices.append(range(idx,idx_end))
+        # vertical win indices
+        for rown in range(1+height-n_connect):
+            for coln in range(width):
+                idx = rown*width + coln
+                idx_end = (rown+n_connect)*width + coln
+                win_indices.append(range(idx,idx_end,width))
+        # up diagonal win indices
+        for rown in range(1+height-n_connect):
+            for coln in range(1+width-n_connect):
+                idx = rown*width + coln
+                idx_end = (rown+n_connect)*width + coln + n_connect
+                win_indices.append(range(idx,idx_end,width+1))
+        # down diagonal win indices
+        for rown in range(1+height-n_connect):
+            for coln in range(n_connect-1,width):
+                idx = rown*width + coln
+                idx_end = (rown+n_connect)*width + coln - n_connect
+                win_indices.append(range(idx,idx_end,width-1))
+        self.__win_indices = win_indices
+        self.__winner = None
+
+    @property
+    def win_indices(self):
+        return self.__win_indices
+
+    @property
+    def winner(self):
+        return self.__winner
+
+    @property
+    def history(self):
+        return self.__history
+
+    @property
+    def n_players(self):
+        return self.__n_players
+
+    def _check_win(self):
+        for wini in self.__win_indices:
+            sb = [self.__board[x] for x in wini]
+            if sb[0] != " " and all([x==sb[0] for x in sb[1:]]):
+                return int(sb[0])
+        return None
+
+    @property
+    def valid_moves(self):
+        if self.winner is not None:
+            return []
+        else:
+            return [idx for idx, spot in enumerate(self.__board[((self.__height-1)*self.__width):]) if spot==" "]
+
+    def _check_win(self):
+        for wini in self.__win_indices:
+            sb = [self.__board[x] for x in wini]
+            if all([x=="0" for x in sb]):
+                return 0
+            elif all([x=="1" for x in sb]):
+                return 1
+        return None
+
+    def play(self, move):
+        if move not in self.valid_moves:
+            raise InvalidMoveException
+        if self.winner is None:
+            rown = 0
+            while self.__board[rown*self.__width+move] != " ":
+                rown += 1
+            self.__board[rown*self.__width+move] = str(self.turn)
+            self.__history.append((self.turn, move))
+            # check for win state
+            self.__winner = self._check_win()
+            if not self.__winner:
+                self._advance()
+
+    def backtrack(self):
+        if len(self.__history) > 0:
+            last_turn, last_move = self.__history.pop()
+            rown = self.__height-1
+            while self.__board[rown*self.__width+last_move] == " ":
+                rown -= 1
+            self.__board[rown*self.__width+last_move] = " "
+            self._retreat()
+            # check for win state; probably safe to just set to None.
+            self.__winner = self._check_win()
+            return (last_turn, last_move)
+        else:
+            return None
+
+    def __repr__(self):
+        bs = ""
+        for idx in range(self.__height-1,-1,-1):
+            bs += "|" + "|".join(self.__board[(self.__width*idx):(self.__width*(idx+1)-1)])
+            bs += "|\n"
+        return bs
 
 
 
@@ -150,6 +261,18 @@ foo.winner
 
 foo.backtrack()
 foo.play(7)
+
+
+reload(tb)
+baz = tb.ConnectFourGame()
+[list(x) for x in baz.win_indices]
+print(baz)
+baz.valid_moves
+baz.play(0)
+print(baz)
+baz.play(1)
+print(baz)
+baz.play(2)
 
 
 
