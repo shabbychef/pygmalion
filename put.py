b11d01c6cde9b3d458e37812a611501ca1f433cd
Steven Pav <shabbychef@gmail.com>
2024-06-10 22:58:00 -0700

    work on urn permutator

--- put.py
+++ put.py
@@ -15,7 +15,27 @@
 """
 
 import itertools
-from math import factorial, comb
+from math import factorial, comb, perm
+from collections import Counter
+
+
+def urn_perm(aco: Counter, k:int=1):
+    """
+    A generator over k-tuples of elements selected without replacement from the given counts.
+    This is a permutation generator, so the order matters.
+    """
+    total_count = sum(aco.values())
+    assert k <= total_count, f"cannot permute {k=} objects from {total_count}"
+    deno = perm(total_count, k)
+    for atup in itertools.product(aco.keys(), repeat=k):
+        wt = 1
+        acop = dict(aco).copy()
+        for av in atup:
+            wt *= acop[av]
+            acop[av] -= 1
+        if wt != 0:
+            yield (*atup, wt/deno)
+
 
 def _score_trick(play1, play2):
     """
@@ -50,6 +70,8 @@ def _score_match(trick1, trick2, trick3):
         else:
             return 0
 
+def _score_match_from(mypl1, mypl2, mypl3, thpl1, thpl2, thpl3):
+    return _score_match(_score_trick(mypl1, thpl1), _score_trick(mypl2, thpl2), _score_trick(mypl3, thpl3))
 
 def second_trick_follower_value():
     """
@@ -70,7 +92,8 @@ def second_trick_follower_value():
             second_trick = _score_trick(mypl2, thpl2)
             deno = 0
             numr = 0
-            for thun1, wt in enumerate(unpl):
+            aco = Counter(dict(list(enumerate(unpl))))
+            for thun1, wt in urn_perm(aco, 1):
                 third_trick = _score_trick(myun1, thun1)
                 numr += wt * _score_match(first_trick, second_trick, third_trick)
                 deno += wt
@@ -100,16 +123,6 @@ def second_trick_follower_decision(secf=None):
 secf = second_trick_follower_value()
 secfd = second_trick_follower_decision(secf)
 
-# compute how many ways you could draw from a given number of urns, without
-# replacement. This is a multinomial coefficient?
-def _comp_wt(urn_counts, *args):
-    dups = [0] * len(urn_counts)
-    for apick in [*args]:
-        dups[apick] += 1
-    wt = 1
-    for idx, ntake in enumerate(dups):
-        wt *= comb(urn_counts[idx], ntake)
-    return wt
 
 def second_trick_leader_value(secfd=None):
     """
@@ -133,10 +146,10 @@ def second_trick_leader_value(secfd=None):
         unpl[thpl1] -= 1
         if min(unpl) >= 0:
             first_trick = _score_trick(mypl1, thpl1)
-            deno = 0
+            aco = Counter(dict(list(enumerate(unpl))))
             numr = 0
-            for thun1, thun2 in itertools.product(range(13), repeat=2):
-                wt = _comp_wt(unpl, thun1, thun2)
+            deno = 0
+            for thun1, thun2, wt in urn_perm(aco, 2):
                 if wt <= 0:
                     continue
                 # figure out what they follow with:
@@ -174,14 +187,17 @@ def second_trick_leader_decision(secl=None):
     return secld
 
 
-
+# ideally there would be an iterator for selecting k cards from a series of n
+# cards, along with the relative probability weight!
 
 secf = second_trick_follower_value()
 secfd = second_trick_follower_decision(secf)
 secl = second_trick_leader_value(secfd)
 secld = second_trick_leader_decision(secl)
+# uninit'd
+secld = second_trick_leader_decision()
 
-def first_trick_follower_value(secld=None):
+def first_trick_follower_value(secld=None, secfd=None):
     """
     computes a dict which is keyed by [(unplayed1, unplayed2, myplayed1, theirplayed1)] with value
     the conditional expected match value from 'my' POV of following in the first trick with myplayed1.
@@ -191,10 +207,115 @@ def first_trick_follower_value(secld=None):
     """
     if secld is None:
         secld = second_trick_leader_decision()
+    if secfd is None:
+        secfd = second_trick_follower_decision()
     firf = {}
-    for myun1, mypl1, mypl2, thpl1 in itertools.product(range(13), repeat=4):
-        # now what?
-        pass
+    for myun1, myun2, mypl1, thpl1 in itertools.product(range(13), repeat=4):
+        if myun1 < myun2:
+            continue
+        unpl = [4] * 13
+        unpl[myun1] -= 1
+        unpl[myun2] -= 1
+        unpl[mypl1] -= 1
+        unpl[thpl1] -= 1
+        if min(unpl) >= 0:
+            first_trick = _score_trick(mypl1, thpl1)
+            aco = Counter(dict(list(enumerate(unpl))))
+            numr = 0
+            deno = 0
+            for thun1, thun2, wt in urn_perm(aco, 2):
+                if wt <= 0:
+                    continue
+                if first_trick > 0:
+                    # we lead in the second trick
+                    # what should we lead with?
+                    _, mypl2 = secld[(max(myun1, myun2), min(myun1, myun2), mypl1, thpl1)]
+                    # what should they follow in the second trick with?
+                    _, thpl2 = secfd[(max(thun1, thun2), min(thun1, thun2), thpl1, mypl1, mypl2)]
+                else:
+                    # first trick we tied or lost after following, so we follow
+                    # in the second.
+                    # figure out what they would lead with
+                    _, thpl2 = secld[(max(thun1, thun2), min(thun1, thun2), thpl1, mypl1)]
+                    # what should we follow with in the second trick?
+                    _, mypl2 = secfd[(max(myun1, myun2), min(myun1, myun2), mypl1, thpl1, thpl2)]
+                mypl3 = myun1 if mypl2 == myun2 else myun2
+                thpl3 = thun1 if thpl2 == thun2 else thun2
+                second_trick = _score_trick(mypl2, thpl2)
+                third_trick = _score_trick(mypl3, thpl3)
+                numr += wt * _score_match(first_trick, second_trick, third_trick)
+                deno += wt
+        firf[(myun1, myun2, mypl1, thpl1)] = numr / deno
+    return firf
+
+firf = first_trick_follower_value(secld, secfd)
+
+def first_trick_follower_decision(firf=None):
+    """
+    computes a dict which is keyed by [(unplayed1, unplayed2, unplayed3, theirplayed1)] with value
+    the conditional expected match value from 'my' POV of following in the first trick, and the optimal card to play.
+    the expected value is _conditional_ on those cards having been played, and everyone playing the optimal
+    decisions.
+    By assumption unplayed1 >= unplayed2 and unplayed2 >= unplayed3
+    """
+    if firf is None:
+        firf = first_trick_follower_value()
+    firfd = {}
+    for myun1, myun2, myun3, thpl1 in itertools.product(range(13), repeat=4):
+        if myun1 < myun2:
+            continue
+        if myun2 < myun3:
+            continue
+        # value from playing 1, 2 or 3
+        val1 = firf[(myun2, myun3, myun1, thpl1)]
+        val2 = firf[(myun1, myun3, myun2, thpl1)]
+        val3 = firf[(myun1, myun2, myun3, thpl1)]
+        firfd[(myun1, myun2, myun3, thpl1)] = max([(val1, myun1), (val2, myun2), (val3, myun3)])
+    return firfd
+
+firfd = first_trick_follower_decision(firf)
+
+# now the first trick leader value.
+def first_trick_leader_value(firfd=None, secld=None, secfd=None):
+    """
+    computes a dict which is keyed by [(unplayed1, unplayed2, myplayed1)] with value
+    the conditional expected match value from 'my' POV of following in the first trick with myplayed1.
+    the expected value is _conditional_ on those cards having been played, and everyone playing the optimal
+    decisions.
+    By assumption unplayed1 >= unplayed2
+    """
+    if secld is None:
+        secld = second_trick_leader_decision()
+    if secfd is None:
+        secfd = second_trick_follower_decision()
+    if firfd is None:
+        firf = first_trick_follower_value(secld, secfd)
+        firfd = first_trick_follower_decision(firf)
+    firl = {}
+    for myun1, myun2, mypl1 in itertools.product(range(13), repeat=3):
+        if myun1 < myun2:
+            continue
+        unpl = [4] * 13
+        unpl[myun1] -= 1
+        unpl[myun2] -= 1
+        unpl[mypl1] -= 1
+        aco = Counter(dict(list(enumerate(unpl))))
+        numr = 0
+        deno = 0
+        for thun1, thun2, thun3, wt in urn_perm(aco, 3):
+            if wt <= 0:
+                continue
+            # figure out what they would play.
+            breakpoint()
+            mypl3 = myun1 if mypl2 == myun2 else myun2
+            thpl3 = thun1 if thpl2 == thun2 else thun2
+            second_trick = _score_trick(mypl2, thpl2)
+            third_trick = _score_trick(mypl3, thpl3)
+            numr += wt * _score_match(first_trick, second_trick, third_trick)
+            deno += wt
+
+        firl[(myun1, myun2, mypl1)] = numr / deno
+    return firl
 
 
 #for vim modeline: (do not edit)
